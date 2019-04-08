f41fd2a583315a0d8274182c6361e6d9c7bfd6bc
Matt Robenolt <matt@ydekproductions.com>
2019-04-08 15:49:15 -0700

    Fix build script

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ mkdir bin
 CROSSPLATFORMS='linux/amd64 darwin/amd64'
 
 for platform in ${CROSSPLATFORMS}; do
-  GOOS=${platform%/*}
-  GOARCH=${platform##*/}
-  go build -v -o bin/gslock-${GOOS}-${GOARCH}
+  GOOS=${platform%/*} \
+  GOARCH=${platform##*/} \
+    go build -v -o bin/gslock-${platform%/*}-${platform##*/}
 done
